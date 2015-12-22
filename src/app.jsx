447a1ef16cf2b16112007ec5500e73ba792f2d3c
Anthony Ho <ahomnky11@gmail.com>
2015-12-22 01:03:38 +0000

    react app

---
+++ src/app.jsx
@@ -0,0 +1,46 @@
+var React = require('react');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var Header = require('./header');
+var List = require('./list');
+var rootUrl = 'https://popping-torch-5894.firebaseio.com/';
+
+var App = React.createClass({
+  mixins: [ ReactFire ],
+  getInitialState: function() {
+    return {
+      items: {},
+      loaded:false
+    }
+  },
+  componentWillMount: function() {
+    fb = new Firebase(rootUrl + 'items/');
+    this.bindAsArray(fb, 'items');
+    fb.on('value', this.handleDataLoaded);
+
+  },
+
+  render: function() {
+    console.log(this.state);
+    return <div className="row panel panel-default">
+        <div className="col-md-8 col-md-offset-2">
+          <h2 className="text-center">
+            To-Do List
+          </h2>
+
+          <Header itemsStore={this.firebaseRefs.items}/>
+          <hr/>
+          <div className={"content " + (this.state.loaded ? 'loaded' : '')}>
+          <List items={this.state.items} />
+          </div>
+        </div>
+        </div>
+  },
+
+  handleDataLoaded: function() {
+    this.setState({loaded: true});
+  }
+});
+
+var element = React.createElement(App, {});
+React.render(element, document.querySelector('.container'));
---
+++ src/list.jsx
@@ -0,0 +1,43 @@
+var React = require('react');
+var ListItem = require('./list-item')
+
+
+module.exports = React.createClass({
+  render:function() {
+
+    return <div>
+      {this.renderList()}
+    </div>
+  },
+  renderList: function() {
+    if(this.props.items && Object.keys(this.props.items).length === 0) {
+      return <h4>
+        Add a todo to get started.
+      </h4>
+    } else {
+      var children = [];
+
+      for (var key in this.props.items) {
+        var item = this.props.items[key];
+        key = item.key;
+
+        children.push(
+          <ListItem
+            item = {item}
+            key = {key}
+             >
+          </ListItem>
+        )
+      }
+
+      return children;
+    }
+  }
+});
+
+//each key represents a single todo.
+//loop over all the key
+//each key, access the object it represents
+//and the access the text propert
+//puts inside the <li> tag
+//puts inside a children and the renderered in the <ul> tag
